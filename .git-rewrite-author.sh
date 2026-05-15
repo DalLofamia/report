6b440872075949e1b8c9b52906652e15890a36d8
Dale Pantano <dalepatano54@gmail.com>
2026-05-15 14:02:06 +0800

    chore: add git rewrite script for author change

---
+++ .git-rewrite-author.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if [ "$GIT_AUTHOR_EMAIL" = "juliuslobaton9021@gmail.com" ]; then
+  export GIT_AUTHOR_NAME="Dale Pantano"
+  export GIT_AUTHOR_EMAIL="dalepatano54@gmail.com"
+fi
+if [ "$GIT_COMMITTER_EMAIL" = "juliuslobaton9021@gmail.com" ]; then
+  export GIT_COMMITTER_NAME="Dale Pantano"
+  export GIT_COMMITTER_EMAIL="dalepatano54@gmail.com"
+fi
